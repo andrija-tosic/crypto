abc401fa1ea89630d85b9b65f5563bbe07fe7257
andrija-tosic <andonnet@gmail.com>
2022-11-30 03:57:55 +0100

    add: Four Square Cipher (bug: writes to file twice)

--- dirt mud djubre/Crypt/Crypt/OneTimePad.h
+++ dirt mud djubre/Crypt/Crypt/OneTimePad.h
@@ -4,23 +4,25 @@
 #include <string>
 #include <fstream>
 
-namespace OneTimePad
+class OneTimePad
 {
-	void Encrypt(const std::string& filePath, const std::string& outFilePath, const std::string& padKeyFilePath) {
+	static constexpr size_t BUF_SIZE = 1024;
+
+public:
+	static void Encrypt(const std::string& filePath, const std::string& outFilePath, const std::string& padKeyFilePath) {
 		std::basic_ifstream<uint8_t> file(filePath, std::ios::in | std::ios::binary);
 		std::basic_ofstream<uint8_t> outfile(outFilePath, std::ios::out | std::ios::binary);
 		std::basic_ofstream<uint8_t> outPadFile(padKeyFilePath, std::ios::out | std::ios::binary);
 
-		constexpr size_t BUF_SIZE = 1024;
-
-		uint8_t outBuf[BUF_SIZE];
-		uint8_t pad[BUF_SIZE];
 
 		while (file.good()) {
+			uint8_t pad[BUF_SIZE];
+			uint8_t outBuf[BUF_SIZE];
 			uint8_t buf[BUF_SIZE];
+
 			file.read(buf, BUF_SIZE);
 
-			for (int i = 0; i < BUF_SIZE; i++) {
+			for (size_t i = 0; i < BUF_SIZE; i++) {
 				pad[i] = rand() % 256;
 				outBuf[i] = buf[i] ^ pad[i];
 			}
@@ -34,22 +36,20 @@ namespace OneTimePad
 		outPadFile.close();
 	}
 
-	void Decrypt(const std::string& filePath, const std::string& outFilePath, const std::string& padKeyFilePath) {
+	static void Decrypt(const std::string& filePath, const std::string& outFilePath, const std::string& padKeyFilePath) {
 		std::basic_ifstream<uint8_t> file(filePath, std::ios::in | std::ios::binary);
 		std::basic_ofstream<uint8_t> outfile(outFilePath, std::ios::out | std::ios::binary);
-		std::basic_ifstream<uint8_t> inPadfile(padKeyFilePath, std::ios::in | std::ios::binary);
-
-		constexpr size_t BUF_SIZE = 1024;
+		std::basic_ifstream<uint8_t> inPadFile(padKeyFilePath, std::ios::in | std::ios::binary);
 
 		uint8_t outBuf[BUF_SIZE];
-		uint8_t pad[BUF_SIZE];
 
-		while (file.good() && inPadfile.good()) {
+		while (file.good() && inPadFile.good()) {
+			uint8_t pad[BUF_SIZE];
 			uint8_t buf[BUF_SIZE];
 			file.read(buf, BUF_SIZE);
-			inPadfile.read(pad, BUF_SIZE);
+			inPadFile.read(pad, BUF_SIZE);
 
-			for (int i = 0; i < BUF_SIZE; i++) {
+			for (size_t i = 0; i < BUF_SIZE; i++) {
 				outBuf[i] = buf[i] ^ pad[i];
 			}
 
@@ -58,7 +58,6 @@ namespace OneTimePad
 
 		file.close();
 		outfile.close();
-		inPadfile.close();
+		inPadFile.close();
 	}
 };
-
--- dirt mud djubre/Crypt/Crypt/main.cpp
+++ dirt mud djubre/Crypt/Crypt/main.cpp
@@ -1,10 +1,13 @@
+#include "FourSquareCipher.h"
 #include "OneTimePad.h"
 
 int main() {
 
 	OneTimePad::Encrypt("D:\\Desktop\\dr dre.jpg", "D:\\Desktop\\dr dre encrypted.jpg", "D:\\Desktop\\otp.key");
-
 	OneTimePad::Decrypt("D:\\Desktop\\dr dre encrypted.jpg", "D:\\Desktop\\dr dre decrypted.jpg", "D:\\Desktop\\otp.key");
+
+	FourSquareCipher::Encrypt("D:\\Desktop\\a.txt", "D:\\Desktop\\fsc.txt");
+	FourSquareCipher::Decrypt("D:\\Desktop\\fsc.txt", "D:\\Desktop\\b.txt");
 
 	return 0;
 }
